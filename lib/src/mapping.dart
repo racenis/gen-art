10c94e824f179a681ed2ba42cdfa5ae184cb3ef7
racenis <martinsrasaolaslasa@gmail.com>
2025-10-19 20:05:25 +0300

    Added mapping file

---
+++ lib/src/mapping.dart
@@ -0,0 +1,11 @@
+class Mapping {
+
+	/*
+		the mapping takes an rgba value at a xy position in the source image and
+		maps it to an rgba' value into xy' position in the destination image
+
+		alternatively it should accept a gradient as well
+		
+	*/
+
+}
